af2bbfe3f094817b1474f4aa6d70b8cc85f8e9fa
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-07-11 00:06:29 -0300

    feat: added button and logic for favorit recipe in localStorage

--- src/Pages/RecipeDetails.jsx
+++ src/Pages/RecipeDetails.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import copy from 'clipboard-copy';
+import imgcompt from '../images/shareIcon.svg';
 
 function RecipeDetails() {
   const [foodData, setFoodData] = useState(null);
@@ -7,7 +10,9 @@ function RecipeDetails() {
   const [recomendsMeals, setRecomendsMeals] = useState([]);
   const [recomendsDrinks, setRecomendsDrinks] = useState([]);
   const [recipeStatus, setRecipeStatus] = useState(false);
+  const [linkCopy, setLinkCopy] = useState(false);
   const { id } = useParams();
+  const history = useHistory();
 
   useEffect(() => {
     const fetchFood = () => {
@@ -70,6 +75,7 @@ function RecipeDetails() {
     strAlcoholic,
     strInstructions,
     strYoutube,
+    strArea,
   } = recipe;
 
   // condição que verifica se a receita é de comida ou bebida;
@@ -83,6 +89,42 @@ function RecipeDetails() {
   ));
   // variavel para quantia de medidas;
   const measure = 13;
+
+  // função para alterar rota
+  const handleStartRecipe = () => {
+    if (recipeType === 'meal') {
+      history.push(`/meals/${id}/in-progress`);
+    } else if (recipeType === 'drink') {
+      history.push(`/drinks/${id}/in-progress`);
+    }
+  };
+
+  // função para copiar link
+  const handleShareClick = () => {
+    const recipeLink = window.location.href;
+    copy(recipeLink);
+    setLinkCopy(true);
+  };
+
+  // função para favoritar receita no LS
+
+  const handleFavoriteRecipe = () => {
+    const favoriteRecipe = {
+      id,
+      type: recipeType === 'meal' ? 'meal' : 'drink',
+      nationality: strArea || '',
+      category: strCategory || '',
+      alcoholicOrNot: strAlcoholic || '',
+      name: strMeal || strDrink || '',
+      image: strMealThumb || strDrinkThumb || '',
+    };
+
+    // tem uma função no projeto shopping cart bem parecida caso queiram olhar
+    let favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+    favoriteRecipes = [...favoriteRecipes, favoriteRecipe];
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
+  };
+
   return (
     <div className="recipes">
       <div className="recipes-content">
@@ -96,7 +138,7 @@ function RecipeDetails() {
           <h2 data-testid="recipe-category">{strMeal ? strCategory : strAlcoholic}</h2>
         </div>
         <h3>Ingredients:</h3>
-        {console.log(recipeARR)}
+        {console.log(recipe)}
         <ul>
           {ingredients.map(([key, value], index) => (
             <li
@@ -166,9 +208,32 @@ function RecipeDetails() {
         </div>
       )}
       <div className="btn-recipe">
-        <button type="button" data-testid="start-recipe-btn">
+        <button
+          onClick={ () => handleStartRecipe() }
+          type="button"
+          data-testid="start-recipe-btn"
+        >
           {recipeStatus ? 'Continue Recipe' : 'Start Recipe'}
         </button>
+        <button
+          data-testid="share-btn"
+          type="button"
+          className="compartilhar"
+          onClick={ () => handleShareClick() }
+        >
+          <img src={ imgcompt } alt="compartilhar" />
+
+        </button>
+        <button data-testid="favorite-btn" type="button" onClick={ handleFavoriteRecipe }>
+          Favoritar
+        </button>
+
+      </div>
+      <div className="link-copy">
+        {linkCopy && (
+          <p>Link copied!</p>
+        )}
+
       </div>
     </div>
   );
