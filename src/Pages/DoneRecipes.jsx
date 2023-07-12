7faac7955a6ed9e0bf948a4dcfd9274566472fe2
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-07-12 20:22:10 -0300

    feat: scrreen DoneRecipes

--- src/Pages/DoneRecipes.jsx
+++ src/Pages/DoneRecipes.jsx
@@ -1,27 +1,36 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import shareIcon from '../images/shareIcon.svg';
+import { copyLink } from '../utils/CopyLinkFunction';
+import { redirectToRecipe } from '../utils/NavigateFunction';
+import Header from '../components/Header';
 
-function FavoriteRecipes() {
+function DoneRecipes() {
   const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
   const [filter, setFilter] = useState('all');
+  const [linkCopy, setLinkCopy] = useState(false);
+  const URLBASE = window.location.origin;
+  const history = useHistory();
 
   const handleFilterChange = (newFilter) => {
     setFilter(newFilter);
-    console.log(newFilter);
   };
 
   const filteredRecipes = filter === 'all'
     ? doneRecipes
     : doneRecipes.filter((recipe) => recipe.type === filter);
+
   const buttonFilter = ['all', 'meal', 'drink'];
 
   return (
     <div>
+      <Header title="Profile" iconeProfile search={ false } />
       {filteredRecipes.length === 0 && <p>Nenhuma receita favorita encontrada.</p>}
       <div>
-        { filteredRecipes.length > 1 && buttonFilter.map((type) => (
+        {console.log(filteredRecipes)}
+        { filteredRecipes.length > 0 && buttonFilter.map((type) => (
           <button
-            key={ type.id }
+            key={ type }
             data-testid={ `filter-by-${type}-btn` }
             onClick={ () => handleFilterChange(type) }
             disabled={ filter === type }
@@ -33,11 +42,13 @@ function FavoriteRecipes() {
 
       {filteredRecipes.map((recipe, index) => (
         <div key={ index }>
-          <img
-            src={ recipe.image }
-            alt="Recipe"
-            data-testid={ `${index}-horizontal-image` }
-          />
+          <button onClick={ () => redirectToRecipe(recipe, history) }>
+            <img
+              src={ recipe.image }
+              alt="Recipe"
+              data-testid={ `${index}-horizontal-image` }
+            />
+          </button>
           <p data-testid={ `${index}-horizontal-top-text` }>
             {recipe.nationality}
             {' '}
@@ -46,10 +57,19 @@ function FavoriteRecipes() {
             {recipe.category}
 
           </p>
-          <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+          <button onClick={ () => redirectToRecipe(recipe, history) }>
+            <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+          </button>
           <p data-testid={ `${index}-horizontal-done-date` }>{recipe.date}</p>
           <p data-testid={ `${index}-horizontal-top-text` }>{recipe.alcoholicOrNot}</p>
-          <button data-testid={ `${index}-horizontal-share-btn` } src={ shareIcon }>
+          <button
+            onClick={ () => {
+              copyLink(`${URLBASE}/${recipe.type}s/${recipe.id}`);
+              setLinkCopy(true);
+            } }
+            data-testid={ `${index}-horizontal-share-btn` }
+            src={ shareIcon }
+          >
             <img src={ shareIcon } alt="compartilhar" />
           </button>
           <p data-testid={ `${index}-horizontal-done-date` }>{ recipe.doneDate }</p>
@@ -64,8 +84,11 @@ function FavoriteRecipes() {
           ))}
         </div>
       ))}
+      <div data-testid="link">
+        {linkCopy && <p>Link copied!</p>}
+      </div>
     </div>
   );
 }
 
-export default FavoriteRecipes;
+export default DoneRecipes;
--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { screen, fireEvent } from '@testing-library/react';
-import FavoriteRecipes from '../Pages/DoneRecipes';
+import DoneRecipes from '../Pages/DoneRecipes';
 import renderWithRouterAndProvider from './helpers/renderWithRouterAndProvider';
 
-describe('FavoriteRecipes', () => {
+jest.mock('clipboard-copy');
+
+describe('DoneRecipes', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     localStorage.clear();
@@ -36,11 +38,15 @@ describe('FavoriteRecipes', () => {
 
   const name = '0-horizontal-name';
   const name2 = '1-horizontal-name';
+  const image = '0-horizontal-image';
+  const filterMealBtn = 'filter-by-meal-btn';
+  const filterDrinkBtn = 'filter-by-drink-btn';
+  const shareBtn = '0-horizontal-share-btn';
 
   test('Deve exibir todas as receitas favoritas quando o filtro for "all"', () => {
     localStorage.setItem('doneRecipes', JSON.stringify([recipe1, recipe2]));
 
-    renderWithRouterAndProvider(<FavoriteRecipes />);
+    renderWithRouterAndProvider(<DoneRecipes />);
 
     const recipe1Name = screen.getByTestId(name);
     const recipe2Name = screen.getByTestId(name2);
@@ -52,7 +58,7 @@ describe('FavoriteRecipes', () => {
   test('Deve exibir uma mensagem quando não houver receitas favoritas', () => {
     localStorage.removeItem('doneRecipes');
 
-    renderWithRouterAndProvider(<FavoriteRecipes />);
+    renderWithRouterAndProvider(<DoneRecipes />);
 
     const emptyMessage = screen.getByText('Nenhuma receita favorita encontrada.');
     expect(emptyMessage).toBeInTheDocument();
@@ -61,9 +67,9 @@ describe('FavoriteRecipes', () => {
   test('Deve filtrar e exibir apenas as receitas de tipo "meal" quando o filtro for "meal"', () => {
     localStorage.setItem('doneRecipes', JSON.stringify([recipe1, recipe2]));
 
-    renderWithRouterAndProvider(<FavoriteRecipes />);
+    renderWithRouterAndProvider(<DoneRecipes />);
 
-    const filterButton = screen.getByTestId('filter-by-meal-btn');
+    const filterButton = screen.getByTestId(filterMealBtn);
     fireEvent.click(filterButton);
 
     const recipe1Name = screen.getByTestId(name);
@@ -76,9 +82,9 @@ describe('FavoriteRecipes', () => {
   test('Deve filtrar e exibir apenas as receitas de tipo "drink" quando o filtro for "drink"', () => {
     localStorage.setItem('doneRecipes', JSON.stringify([recipe1, recipe2]));
 
-    renderWithRouterAndProvider(<FavoriteRecipes />);
+    renderWithRouterAndProvider(<DoneRecipes />);
 
-    const filterButton = screen.getByTestId('filter-by-drink-btn');
+    const filterButton = screen.getByTestId(filterDrinkBtn);
     fireEvent.click(filterButton);
 
     const recipe2Name = screen.getByTestId(name);
@@ -86,5 +92,41 @@ describe('FavoriteRecipes', () => {
 
     expect(recipe2Name).toHaveTextContent('Recipe 2');
     expect(recipe1Name).toBeNull();
+  });
+
+  test('Deve redirecionar para a página correta ao clicar em nome de receita', () => {
+    localStorage.setItem('doneRecipes', JSON.stringify([recipe1, recipe2]));
+
+    const { history } = renderWithRouterAndProvider(<DoneRecipes />);
+
+    const recipe1Name = screen.getByTestId(name);
+    fireEvent.click(recipe1Name);
+
+    expect(history.location.pathname).toBe('/meals/1');
+  });
+
+  test('Deve redirecionar para a página correta ao clicar na imagem de receita', () => {
+    localStorage.setItem('doneRecipes', JSON.stringify([recipe1, recipe2]));
+
+    const { history } = renderWithRouterAndProvider(<DoneRecipes />);
+
+    const recipe1Image = screen.getByTestId(image);
+    fireEvent.click(recipe1Image);
+
+    expect(history.location.pathname).toBe('/meals/1');
+  });
+
+  test('Texto de link de receita deve aparecer ao clicar no botão de compartilhar', () => {
+    localStorage.setItem('doneRecipes', JSON.stringify([recipe1, recipe2]));
+
+    renderWithRouterAndProvider(<DoneRecipes />);
+
+    const link = screen.getByTestId('link');
+    expect(link).not.toHaveTextContent('Link copied!');
+
+    const btnShare = screen.getByTestId(shareBtn);
+    fireEvent.click(btnShare);
+
+    expect(link).toHaveTextContent('Link copied!');
   });
 });
