4bfcece894a0d3e7fbf00a8d960b8083da440877
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-07-13 16:34:59 -0300

    feat: tests page RecipeDetail

--- src/tests/RecipeDetail.test.js
+++ src/tests/RecipeDetail.test.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouterAndProvider from './helpers/renderWithRouterAndProvider';
-import { dataDrinks, dataMeals } from './helpers/data';
+import { dataDrinks, dataMeals, recommendedDataMock } from './helpers/data';
 import RecipeDetails from '../Pages/RecipeDetails';
 import App from '../App';
+import RecipeCarousel from '../components/RecipeCarousel';
 
 describe('Teste do componente RecipeDetail', () => {
   beforeEach(() => {
@@ -12,9 +13,39 @@ describe('Teste do componente RecipeDetail', () => {
     localStorage.clear();
   });
 
+  jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValueOnce({ meals: recommendedDataMock }),
+  });
+
   const startBtn = 'start-recipe-btn';
 
-  test('se vai para a rota correta ao clicar no botão start recipe para meals', async () => {
+  test('Verifica se API recommended retorna', async () => {
+    renderWithRouterAndProvider(
+      <RecipeCarousel recommendations={ recommendedDataMock } />,
+    );
+
+    expect(screen.getByTestId('0-recommendation-card')).toBeInTheDocument();
+    recommendedDataMock.slice(0, 5).forEach((item, index) => {
+      expect(screen.getByTestId(`${index}-recommendation-card`)).toBeInTheDocument();
+    });
+  });
+
+  test('Verifica se função é chamada', () => {
+    const handleFavoriteRecipe = jest.fn();
+
+    renderWithRouterAndProvider(<RecipeDetails
+      handleFavoriteRecipe={ handleFavoriteRecipe }
+    />);
+
+    const favoriteButton = screen.getByTestId('favorite-btn');
+
+    expect(favoriteButton).toBeInTheDocument();
+
+    userEvent.click(favoriteButton);
+
+    expect(handleFavoriteRecipe).toHaveBeenCalledTimes(0);
+  });
+  test('Verifica se vai para a rota correta ao clicar no botão start recipe para meals', async () => {
     jest.fn().mockResolvedValueOnce([dataMeals, dataDrinks]);
     const id = '52771';
 
@@ -27,7 +58,7 @@ describe('Teste do componente RecipeDetail', () => {
     });
   });
 
-  test('se vai para a rota correta ao clicar no botão start recipe para drinks', async () => {
+  test('Verifica se vai para a rota correta ao clicar no botão start recipe para drinks', async () => {
     jest.fn().mockResolvedValueOnce([dataMeals, dataDrinks]);
     const id = '52771';
 
@@ -40,7 +71,7 @@ describe('Teste do componente RecipeDetail', () => {
     });
   });
 
-  test('testa botão de copiar e verifica se o texto foi copiado', async () => {
+  test('Verifica botão de copiar e verifica se o texto foi copiado', async () => {
     jest.fn().mockResolvedValueOnce([dataMeals, dataDrinks]);
     const id = '52771';
     renderWithRouterAndProvider(<RecipeDetails />, `/meals/${id}`);
@@ -56,5 +87,47 @@ describe('Teste do componente RecipeDetail', () => {
       expect(mockCopy).toHaveBeenCalledWith(`http://localhost/meals/${id}`);
     });
     expect(link).toHaveTextContent(/Link Copied/i);
+  });
+
+  test('Verifica localStorage de favoriteRecipes', () => {
+    const favoriteRecipess = [
+      { id: '52771', type: 'meal' },
+    ];
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipess));
+
+    const { getByTestId } = renderWithRouterAndProvider(<RecipeDetails />);
+    const favoriteButton = getByTestId('favorite-btn');
+
+    expect(favoriteButton).toBeInTheDocument();
+    expect(favoriteButton).toHaveAttribute('src');
+  });
+
+  test('Verifica localStorage de inProgressRecipes', async () => {
+    const inProgressRecipes = {
+      meals: {},
+      drinks: {
+        178319: [
+          {
+            id: '178319',
+            type: 'drink',
+            nationality: '',
+            category: 'Cocktail',
+            alcoholicOrNot: 'Alcoholic',
+            name: 'Aquamarine',
+            image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+            doneDate: '23/6/2020',
+            tags: [],
+          },
+        ],
+      },
+    };
+    localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipes));
+
+    const { getByTestId } = renderWithRouterAndProvider(<App />, '/drinks/178319/');
+    const startRecipeButton = getByTestId('start-recipe-btn');
+
+    await waitFor(() => {
+      expect(startRecipeButton).toHaveTextContent('Continue Recipe');
+    });
   });
 });
