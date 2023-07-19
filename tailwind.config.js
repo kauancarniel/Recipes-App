e1f3594f422c2499bc7e8581eb8645ffa0d776a4
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-07-18 22:21:15 -0300

    config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './src/**/*.{js,jsx,ts,tsx}',
+];
+export const theme = {
+  extend: {},
+  fontFamily: {
+    epilogue: ['Epilogue', 'sans-serif'],
+  },
+  screens: {
+    sm: '394px',
+    md: '768px',
+    lg: '1024px',
+  },
+};
+export const plugins = [];
