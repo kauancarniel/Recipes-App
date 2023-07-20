e4badcd4f6efc1b83a184f808df71bc05d3edffc
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-07-20 14:23:55 -0300

    test carousel

--- src/components/RecommendRecipes.jsx
+++ src/components/RecommendRecipes.jsx
@@ -1,8 +1,42 @@
+// import React from 'react';
+// import { useLocation } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+// import { Carousel } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import './RecommendRecipes.css';
+
+// function RecommendRecipes({ recommendRecipes }) {
+//   const { pathname } = useLocation();
+//   const KEY_BASE = pathname.split('/')[1] === 'meals' ? 'Drink' : 'Meal';
+
+//   return (
+//     <Carousel className="mb-10">
+//       {recommendRecipes.map((recipe, index) => (
+//         <Carousel.Item key={ `${recipe[`id${KEY_BASE}`]}${recipe[`str${KEY_BASE}`]}` }>
+//           <div className='bg-black  '>
+//             <h1 className='absolute text-white font-bold left-7 top-3 text-xl'>Recommends</h1>
+//             <img
+//               className="w-[100%] h-[250px] opacity-40  border-2 border-black border-r-0 border-l-0 "
+//               src={ `${recipe[`str${KEY_BASE}Thumb`]}` }
+//               alt={ `${recipe[`str${KEY_BASE}`]}` }
+//               data-testid={ `${index}-recommendation-card` }
+//             />
+//           </div>
+//         </Carousel.Item>
+//       ))}
+//     </Carousel>
+//   );
+// }
+
+// RecommendRecipes.propTypes = {
+//   recommendRecipes: PropTypes.arrayOf(PropTypes.instanceOf(Object)).isRequired,
+// };
+
+// export default RecommendRecipes;
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Carousel } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './RecommendRecipes.css';
 
@@ -11,26 +45,30 @@ function RecommendRecipes({ recommendRecipes }) {
   const KEY_BASE = pathname.split('/')[1] === 'meals' ? 'Drink' : 'Meal';
 
   return (
-    <Carousel className="mb-10">
+    <aside className="caroussel">
       {recommendRecipes.map((recipe, index) => (
-        <Carousel.Item key={ `${recipe[`id${KEY_BASE}`]}${recipe[`str${KEY_BASE}`]}` }>
-          <div className='bg-black  '>
-            <h1 className='absolute text-white font-bold left-7 top-3 text-xl'>Recommends</h1>
-            <img
-              className="w-[100%] h-[250px] opacity-40  border-2 border-black border-r-0 border-l-0 "
-              src={ `${recipe[`str${KEY_BASE}Thumb`]}` }
-              alt={ `${recipe[`str${KEY_BASE}`]}` }
-              data-testid={ `${index}-recommendation-card` }
-            />
-          </div>
-        </Carousel.Item>
+        <div
+          className="recommendation-card"
+          key={ `${recipe[`id${KEY_BASE}`]}${recipe[`str${KEY_BASE}`]}` }
+        >
+          <img
+            className="img-recommend"
+            src={ `${recipe[`str${KEY_BASE}Thumb`]}` }
+            alt={ `${recipe[`str${KEY_BASE}`]}` }
+            data-testid={ `${index}-recommendation-card` }
+          />
+          <h5 data-testid={ `${index}-recommendation-title` }>
+            {`${recipe[`str${KEY_BASE}`]}`}
+          </h5>
+        </div>
       ))}
-    </Carousel>
+    </aside>
   );
 }
 
 RecommendRecipes.propTypes = {
-  recommendRecipes: PropTypes.arrayOf(PropTypes.instanceOf(Object)).isRequired,
+  recommendRecipes: PropTypes.arrayOf(
+    PropTypes.instanceOf(Object),
+  ).isRequired,
 };
-
 export default RecommendRecipes;
