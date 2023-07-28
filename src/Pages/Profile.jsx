7a0f25790221145ba4eba4142f2b3a0e200f3ed6
Kauan Carniel Peres <kauan-carniel@hotmail.com>
2023-07-28 17:34:06 -0300

    fix

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Header from '../components/Header';
 import EditUserInfo from '../components/EditUserInfo';
 
-import './Profile.css';
 import './Login.css';
 
 function Profile() {
