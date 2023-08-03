024b6dab21d7a909efa0a2c4ab8a518eea6ca0d1
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-08-03 17:43:50 -0300

    fix: refatorei

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,6 @@
-/* eslint-disable max-len */
 import React from 'react';
-import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import Header from '../components/Header';
+import Host from '../components/Host';
 import lucasIMG from '../images/lucas.jpeg';
 import guilhermeIMG from '../images/guilherme.jpeg';
 import './About.css';
@@ -11,7 +10,8 @@ function About() {
     {
       name: 'Guilherme Fernandes',
       github: '',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quis rem eum totam fuga, ut soluta vel accusantium explicabo eius fugiat necessitatibus sed molestiae animi officiis quos! Nemo, quae in?',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quis rem eum totam fuga, ut soluta vel accusantium explicabo eius fugiat necessitatibus sed molestiae animi officiis quos! Nemo, quae in?',
       linkedin: '',
       image: guilhermeIMG,
     },
@@ -25,7 +25,8 @@ function About() {
     {
       name: 'Lucas Diello',
       github: 'https://github.com/LucasDiello',
-      description: 'Sempre admirei a tecnologia, pois vivenciei minha infância e minha adolescência em frente a um computador, sou obcecado por desafios e hoje estou aprendendo sobre o mundo do desenvolvimento web, caso interesse entre em contato! 🚀',
+      description:
+        'Sempre admirei a tecnologia, pois vivenciei minha infância e minha adolescência em frente a um computador, sou obcecado por desafios e hoje estou aprendendo sobre o mundo do desenvolvimento web, caso interesse entre em contato!  🚀',
       linkedin: 'https://www.linkedin.com/in/lucas-diello-5b5440265/',
       image: lucasIMG,
     },
@@ -36,64 +37,33 @@ function About() {
       <Header title="About" />
       <main
         className="recipe-box
-       flex bg-form glass box-bottom gap-3 p-10 flex-wrap justify-center"
+      flex bg-form glass box-bottom gap-3 p-10 flex-wrap justify-center"
       >
         <section className="p-2">
-          <h1 className="md:text-start text-center text-white">Title</h1>
+          <h1 className="text-start text-white mb-6">Aplicação</h1>
           <p className="text-[var(--gray)]">
-            Lorem ipsum dolor sit amet consectetur adipisicing eli
-            . Incidunt quis rem eum totam fuga, ut soluta v
-            el accusantium explicabo eius fugiat
-            necessitatibus sed molestiae animi officiis quos!
-            Nemo, quae in?Lorem ipsum dolor,
-            sit amet
-            consectetur adipisicin
-            g elit. Quam maxime repudiandae, cum ut nesciun
-            t, quisquam ex tempora voluptatem
-            nostrum quo assumenda veritatis eos laudantium laborum,
-            eaque officiis blanditiis
-            minus tene
-            tur! Lorem i
-            psum dolor sit, amet consectetur adipisic
-            ing elit. Dolorum corrupti odit min
-            ima atque commod
-            i magnam eos harum, voluptates officia provident quis
-            unde voluptatibus voluptas! T
-            empore culpa sequi consequuntur necessitatibus aperiam?
+            Lorem ipsum dolor
+            sit amet consectetur adipisicing elit.
+            Odio, mollitia! Inventore quae ab recusandae lau
+            dantium, temporibus dicta quibusdam nulla labore o
+            dio aut odit in. Ex voluptatem quis adipisci enim quidem.
+            Lorem ipsum dolor sit amet consectetur a
+            ipisicing elit. Explicabo consequatur non deserunt i
+            psa nihil cupiditate optio ab id animi distinctio, velit
+            laboriosam ipsum fugit quia placeat ullam provident earum consectet
+            ur!
+            Lorem ipsum, dolor sit amet consectetur adipisicing eli
+            t. Architecto hic quod, voluptas accusantium beatae conseq
+            uuntur nam nostrum debitis magni sed, iure consectetur?
+            Repudiandae optio distinctio facilis aut numquam maxime amet?
           </p>
         </section>
         <div className="w-[100%] p-2">
-          <h2 className="text-white md:text-start text-center ">Desenvolvedores</h2>
+          <h2 className="text-white md:text-start text-center">Desenvolvedores</h2>
         </div>
         <div className="flex flex-wrap justify-center">
           {hosts.map((host, index) => (
-            <div className="p-2 mr-2 " key={ index }>
-              <div className="image-container">
-                <img
-                  className="border-div w-[280px] h-[340px] image-about"
-                  src={ host.image }
-                  alt="img-hosts"
-                />
-                <div className="text-about">
-                  <div className="flex justify-between mb-2">
-                    <h5 className="text-[var(--gray)] mr-2">{host.name}</h5>
-                    <div>
-                      {host.linkedin && (
-                        <a href={ host.linkedin } target="_blank" rel="noreferrer">
-                          <AiFillLinkedin size={ 25 } className="text-[var(--orange)] " />
-                        </a>
-                      )}
-                      {host.github && (
-                        <a href={ host.github } target="_blank" rel="noreferrer">
-                          <AiFillGithub size={ 25 } className="text-[var(--orange)] " />
-                        </a>
-                      )}
-                    </div>
-                  </div>
-                  <p>{host.description}</p>
-                </div>
-              </div>
-            </div>
+            <Host key={ index } { ...host } />
           ))}
         </div>
       </main>
