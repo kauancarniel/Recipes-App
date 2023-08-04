e9587adf4db37690b15360d5e84379674657bb85
Kauan Carniel Peres <kauan-carniel@hotmail.com>
2023-08-04 20:18:57 -0300

    feat: not found page

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import img from '../images/imgNotFounded.png';
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div
+      className="flex justify-center items-center my-auto
+      flex-col bg-notFounded glass w-1/2 mx-auto rounded-xl p-10 h-150"
+    >
+      <img src={ img } alt="asd" className="mt-30" />
+      <h1>Page Not Found</h1>
+      <button
+        className="cursor-pointer duration-200 hover:scale-125 active:scale-100"
+        title="Go To Home"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 24 24" className="stroke-blue-300">
+          <path
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="1.5"
+            d="M11 6L5 12M5 12L11 18M5 12H19"
+          />
+        </svg>
+      </button>
+    </div>
   );
 }
