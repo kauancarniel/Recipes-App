49a2c886fc87af7b0b265fb71642140bed9ed4b1
Kauan Carniel Peres <kauan-carniel@hotmail.com>
2023-08-04 20:33:21 -0300

    not found page completa

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import img from '../images/imgNotFounded.png';
+import './NotFound.css';
 
 export default function NotFound() {
+  const history = useHistory();
+  const exit = () => {
+    history.push('/meals');
+  };
   return (
     <div
       className="flex justify-center items-center my-auto
@@ -9,19 +15,7 @@ export default function NotFound() {
     >
       <img src={ img } alt="asd" className="mt-30" />
       <h1>Page Not Found</h1>
-      <button
-        className="cursor-pointer duration-200 hover:scale-125 active:scale-100"
-        title="Go To Homee"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 24 24" className="stroke-blue-300">
-          <path
-            strokeLinejoin="round"
-            strokeLinecap="round"
-            strokeWidth="1.5"
-            d="M11 6L5 12M5 12L11 18M5 12H19"
-          />
-        </svg>
-      </button>
+      <button onClick={ exit }>Back to Home</button>
     </div>
   );
 }
